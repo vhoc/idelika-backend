b08d099ee696dd81d68af2990888fa9a17b570c9
Víctor Olvera <vhocar@gmail.com>
2022-05-12 17:05:00 -0700

    Added Create User

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,16 +2,11 @@ const express = require( 'express' )
 const res = require('express/lib/response')
 const router = express.Router()
 const Usuario = require( `../models/usuario` )
+const bcrypt = require( 'bcrypt' )
 
 /**
  * Always place static routes first, then dynamic routes last.
  */
-
-
-const validation = (request, response, next) => {
-    console.log( `Validation executed` )
-    next()
-}
 
 // Get all
 router.get( '/', async ( request, response ) => {
@@ -51,6 +46,31 @@ router.delete( '/:id', ( request, response ) => {
 // Middleware (useful for server side validations)
 router.param( 'id', ( request, response, next, id ) => {
     next()
+} )
+
+// Tests
+router.post( '/test/', ( request, response ) => {
+    bcrypt.hash( request.body.password, 11).then( hashedPass => {
+        Usuario.create({
+            nombre: request.body.nombre,
+            apellidoPaterno: request.body.apellidoPaterno,
+            apellidoMaterno: request.body.apellidoMaterno,
+            email: request.body.email,
+            password: hashedPass,
+            empresa: request.body.empresa,
+            avatar: request.body.avatar,
+        }).then( newUser => {
+            response.status( 201 ).json( newUser )
+        } ).catch( error => {
+            response.status( 500 ).json( {message: error.message} )
+            console.error( error )
+        } )
+    } ).catch( error => {
+        response.status( 500 ).json( {message: error.message} )
+        console.error( error )
+    } )
+
+    
 } )
 
 module.exports = router
