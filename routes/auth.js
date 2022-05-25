6d229411e06752b27ddb92c80fe087c7b65b674a
Víctor Olvera <vhocar@gmail.com>
2022-05-25 07:20:52 -0700

    Added tocken check

--- routes/auth.js
+++ routes/auth.js
@@ -5,30 +5,46 @@ const { generateAccessToken } = require( `../helpers/generateAccessToken` )
 
 const RefreshToken = require( `../models/refreshToken` )
 
-// Update Token
+// Verify Token
 router.post( '/token', async ( request, response ) => {
-    const refreshToken = request.body.token
+    const authHeader = request.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return response.sendStatus(401)
+
+    jwt.verify( token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
+        if ( error ) return response.sendStatus(403)
+        request.user = user
+        next()
+    } )
+} )
+
+// Refresh Token
+router.post( '/refreshtoken', async ( request, response ) => {
+    const refreshToken = request.body.refreshToken
     if ( refreshToken == null ) return response.sendStatus( 401 )
 
     const savedToken = await RefreshToken.findOne( { refreshToken }, 'refreshToken' )
-    if ( refreshToken !== savedToken.refreshToken || !savedToken.refreshToken ) response.sendStatus( 403 )
+    if ( savedToken && refreshToken !== savedToken.refreshToken  ) return response.sendStatus( 403 )
 
     jwt.verify( refreshToken, process.env.REFRESH_TOKEN_SECRET, ( error, user ) => {
         if ( error ) return response.sendStatus(403)
         const accessToken = generateAccessToken( { nombre: user.nombre } )
-        response.json({ accessToken })
+        return response.json({ accessToken })
     } )
 } )
 
+// Delete Token
 router.delete( '/logout', async ( request, response ) => {
     const refreshToken = request.body.token
     if ( refreshToken == null ) return response.sendStatus( 401 )
 
     const savedToken = await RefreshToken.findOne( { refreshToken }, 'refreshToken' )
-    if ( refreshToken !== savedToken.refreshToken || !savedToken.refreshToken ) response.sendStatus( 403 )
+    if ( savedToken == null ) return response.sendStatus( 204 )
+    if ( refreshToken !== savedToken.refreshToken ) return response.sendStatus( 403 )
 
     await RefreshToken.deleteOne( { refreshToken: refreshToken } )
-    response.sendStatus( 204 )
+    return response.sendStatus( 204 )
     
 } )
 
