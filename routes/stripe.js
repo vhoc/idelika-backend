6164806696038cfc310cb3916a8408a14d36d21b
Victor Olvera <vhocar@gmail.com>
2022-10-31 16:14:51 -0700

    Register WIP

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,6 +1,7 @@
+require( 'dotenv' ).config()
 const express = require( 'express' )
 const router = express.Router()
-const stripe = require( 'stripe' )( 'sk_test_IKYCHOAmUhC7IPTdaoVtO58D' )
+const stripe = require( 'stripe' )( process.env.STRIPE_SECRET_KEY )
 
 router.post('/payment-sheet', async (req, res) => {
     // Use an existing Customer ID if this is a returning customer.
@@ -22,8 +23,14 @@ router.post('/payment-sheet', async (req, res) => {
       paymentIntent: paymentIntent.client_secret,
       ephemeralKey: ephemeralKey.secret,
       customer: customer.id,
-      publishableKey: 'pk_test_wk6O7Cc5k3McBIG2Hut2irGs'
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
     });
-  });
+});
+
+router.get('countryside', async (req, res) => {
+    res.json({
+        pk: process.env.STRIPE_PUBLISHABLE_KEY
+    })
+})
 
 module.exports = router;
