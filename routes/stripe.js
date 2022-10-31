d357e00fb0401d8f85904a600a706e63f19a9c95
Victor Olvera <vhocar@gmail.com>
2022-10-31 15:11:54 -0700

    Added stripe support

---
+++ routes/stripe.js
@@ -0,0 +1,29 @@
+const express = require( 'express' )
+const router = express.Router()
+const stripe = require( 'stripe' )( 'sk_test_IKYCHOAmUhC7IPTdaoVtO58D' )
+
+router.post('/payment-sheet', async (req, res) => {
+    // Use an existing Customer ID if this is a returning customer.
+    const customer = await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      {customer: customer.id},
+      {apiVersion: '2022-08-01'}
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099,
+      currency: 'eur',
+      customer: customer.id,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+  
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      publishableKey: 'pk_test_wk6O7Cc5k3McBIG2Hut2irGs'
+    });
+  });
+
+module.exports = router
