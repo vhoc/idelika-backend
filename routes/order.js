1bd4f0ea73cc387560b293b70a7f123c4c26f8d5
Victor Olvera <vhocar@gmail.com>
2022-11-02 10:58:01 -0700

    Idelika

--- routes/order.js
+++ routes/order.js
@@ -6,7 +6,7 @@ router.post('/', async (req, res) => {
   try {
     return response.json(req.body)
   } catch (error) {
-    res.status(error.statusCode).json(error)
+    res.json(error)
   }
     
 });
