5cdf54a35e5b850bbbe0841dcdd7d042f2895fa8
Victor Olvera <vhocar@gmail.com>
2022-11-02 14:51:44 -0700

    Idelika

--- routes/ecwid.js
+++ routes/ecwid.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const axios = require('axios')
 
 // Get Ecwid's user details
-router.get('/user/:id', async (req, res) => {
+router.get('/user/:id', async (request, response) => {
   try {
     const ecwidUser = await axios.get( `${process.env.ECWID_API_URL}/customers/${request.params.id}`, {
       method: 'GET',
