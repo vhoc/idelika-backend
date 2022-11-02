3988a32ee5f807c1473d720032dffa78e67c402d
Victor Olvera <vhocar@gmail.com>
2022-11-02 14:54:54 -0700

    Idelika

--- routes/ecwid.js
+++ routes/ecwid.js
@@ -27,7 +27,7 @@ router.get('/user/:id', async (request, response) => {
       "Tier 3": 3,
       "Tier 4": 4,
     }
-    console.log(response.data)
+    console.log(ecwidUser.data)
     return response.status(200).json({
       ecwidUserId: response.data.id,
       email: response.data.email,
