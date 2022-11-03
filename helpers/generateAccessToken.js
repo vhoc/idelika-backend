10255eb096fddc44c32c41721b5e737d66df54db
Victor Olvera <vhocar@gmail.com>
2022-11-03 08:52:14 -0700

    Idelika

--- helpers/generateAccessToken.js
+++ helpers/generateAccessToken.js
@@ -1,7 +1,7 @@
 const jwt = require( 'jsonwebtoken' )
 
 const generateAccessToken = user => {
-    return jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m' } )
+    return jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '300m' } )
 }
 
 module.exports = { generateAccessToken }
--- routes/user.js
+++ routes/user.js
@@ -30,16 +30,11 @@ router.get( '/:id', async ( request, response ) => {
     //response.send( `Get one user: ${ request.params.id }` )
     try {
         const usuario = await Usuario.findById( request.params.id )
-        //const formulario = await Formularios.findOne( { usuarioId: request.params.id } )
-        //const preferencias = await Preferencias.findOne( { usuarioId: request.params.id } )
 
         return response.status( 200 ).json( {
             id: usuario._id,
             nombre: usuario.nombre,
             email: usuario.email,
-            //empresa: usuario.empresa,
-            //formulario,
-            //preferencias
         } )
     } catch ( error ) {
         return response.status( 500 ).json( { status: 500, message: error.message } )
