4a8d86490f392559718fde3b2769865c72a1503a
Victor Olvera <vhocar@gmail.com>
2022-11-08 11:12:02 -0700

    Idelika

---
+++ helpers/ecwidUserData.js
@@ -0,0 +1,32 @@
+require( 'dotenv' ).config()
+const axios = require('axios')
+
+const getUserDiscount = async (customerId, email) => {
+    try {
+        const calculate = await axios.post(`${process.env.ECWID_API_URL}/order/calculate`, {
+            email: email,
+            customerId: customerId,
+            items: [
+              {
+                "productId": 0,
+                "quantity": 1
+              }
+            ]
+        }, {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: process.env.IDELIKA_ACCESS_TOKEN
+          },
+        })
+
+        return { type: calculate.data.discountInfo[0].type, value: calculate.data.discountInfo[0].value }
+    } catch(error) {
+        console.log(`Error fetching user's discount.`)
+        return { type: "PERCENT", value: 0 }
+    }
+    
+}
+
+module.exports = { getUserDiscount }
