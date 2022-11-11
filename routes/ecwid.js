172f57286de9d430dbead7a2e1ae777e60b67d47
Victor Olvera <vhocar@gmail.com>
2022-11-10 17:39:37 -0700

    Idelika

--- routes/ecwid.js
+++ routes/ecwid.js
@@ -159,6 +159,7 @@ router.post(`/available-shipping-methods`, async (request, response) => {
      * An if conditional will do for the city evaluation
      * and a switch statement seems appropiate for the state.
      */
+    console.log(JSON.stringify(address))
     if (
       (address.city.toLowerCase() === 'guadalajara' ||
       address.city.toLowerCase() === 'zapopan' ||
