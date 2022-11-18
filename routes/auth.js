495758b92c514046a9304a4413b4c0f3c346973e
Victor Olvera <vhocar@gmail.com>
2022-11-18 10:32:27 -0700

    Idelika

--- routes/auth.js
+++ routes/auth.js
@@ -98,6 +98,7 @@ router.post( "/password-reset/:usuarioId/:token", async ( req, res ) => {
     try {
         const schema = Joi.object({ password: Joi.string().required() });
         const { error } = schema.validate(req.body);
+        console.log(error)
         if (error) return res.status(400).json({ status: 400, message: error.details[0].message});
 
         const user = await Usuario.findById(req.params.usuarioId);
