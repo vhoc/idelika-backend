73ce2bac87918a87d6c67846e685bcb048e72cd1
Victor Olvera <vhocar@gmail.com>
2022-12-05 16:49:02 -0700

    Removed expiration time for access token

--- helpers/generateAccessToken.js
+++ helpers/generateAccessToken.js
@@ -1,7 +1,8 @@
 const jwt = require( 'jsonwebtoken' )
 
 const generateAccessToken = user => {
-    return jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '300m' } )
+    //return jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '300m' } )
+    return jwt.sign( user, process.env.ACCESS_TOKEN_SECRET )
 }
 
 module.exports = { generateAccessToken }
