586f896c9605a10e0977dfb89719636445de0d38
Victor Olvera <vhocar@gmail.com>
2022-12-05 16:38:15 -0700

    Updated activation link route

--- helpers/generateActivationLink.js
+++ helpers/generateActivationLink.js
@@ -10,10 +10,10 @@ const generateActivationLink = async ( usuario ) => {
         }).save();
     }
 
-    const link = `${process.env.FRONTEND_URL}proxy/auth/activate/${usuario._id}/${token.token}`;
+    const link = `${process.env.FRONTEND_URL}api/auth/activate/${usuario._id}/${token.token}`;
     // TODO: CREAR RUTA /activate/
     return link
 }
 
 module.exports = { generateActivationLink }
-//https://idelika.xneighbor.com/proxy/auth/activate/63619a3c4900f95f40df842f/56975630a6a9983f8fa40accf23cc1ea13772daf77cf03d7bdadbba917766d1c
+//https://idelika.xneighbor.com/api/auth/activate/63619a3c4900f95f40df842f/56975630a6a9983f8fa40accf23cc1ea13772daf77cf03d7bdadbba917766d1c
